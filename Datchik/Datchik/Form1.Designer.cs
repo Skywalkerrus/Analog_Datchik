9201b8ea8e241b0026471e5e51072d7e64b3d88b
Skywalkerrus <40767821+Skywalkerrus@users.noreply.github.com>
2018-07-31 23:47:32 +0300

    Add Datchik

---
+++ Datchik/Datchik/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Datchik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialData = new System.Windows.Forms.RichTextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.button1.Location = new System.Drawing.Point(840, 520);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Monitoring";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // serialData
+            // 
+            this.serialData.Location = new System.Drawing.Point(812, 74);
+            this.serialData.Name = "serialData";
+            this.serialData.Size = new System.Drawing.Size(78, 44);
+            this.serialData.TabIndex = 2;
+            this.serialData.Text = "";
+            this.serialData.TextChanged += new System.EventHandler(this.serialData_TextChanged);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // folderBrowserDialog2
+            // 
+            this.folderBrowserDialog2.HelpRequest += new System.EventHandler(this.folderBrowserDialog2_HelpRequest);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DeepPink;
+            this.label1.Location = new System.Drawing.Point(1005, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Number your port";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1018, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(812, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "Data output";
+            // 
+            // chart2
+            // 
+            this.chart2.BackColor = System.Drawing.Color.DarkCyan;
+            this.chart2.BorderlineColor = System.Drawing.Color.Aqua;
+            chartArea1.AxisX.IsMarginVisible = false;
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart2.Legends.Add(legend1);
+            this.chart2.Location = new System.Drawing.Point(12, 205);
+            this.chart2.Name = "chart2";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.IsXValueIndexed = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Y";
+            this.chart2.Series.Add(series1);
+            this.chart2.Size = new System.Drawing.Size(780, 175);
+            this.chart2.TabIndex = 9;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            this.chart3.BackColor = System.Drawing.Color.DarkCyan;
+            this.chart3.BorderlineColor = System.Drawing.Color.Aqua;
+            chartArea2.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart3.Legends.Add(legend2);
+            this.chart3.Location = new System.Drawing.Point(12, 386);
+            this.chart3.Name = "chart3";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.IsXValueIndexed = true;
+            series2.Legend = "Legend1";
+            series2.Name = "Z";
+            series2.XValueMember = "60";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            series2.YValueMembers = "300";
+            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chart3.Series.Add(series2);
+            this.chart3.Size = new System.Drawing.Size(780, 171);
+            this.chart3.TabIndex = 10;
+            this.chart3.Text = "chart3";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DeepPink;
+            this.button2.Location = new System.Drawing.Point(1008, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 35);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Save File";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(812, 237);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(78, 43);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(812, 414);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(78, 43);
+            this.richTextBox2.TabIndex = 15;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(946, 131);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(162, 172);
+            this.richTextBox3.TabIndex = 16;
+            this.richTextBox3.Text = "     X        Y        Z";
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.DarkCyan;
+            this.chart1.BorderlineColor = System.Drawing.Color.Aqua;
+            chartArea3.AxisX.IsMarginVisible = false;
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(12, 25);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.IsXValueIndexed = true;
+            series3.Legend = "Legend1";
+            series3.Name = "X";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(780, 175);
+            this.chart1.TabIndex = 17;
+            this.chart1.Text = "chart1";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.BlueViolet;
+            this.button4.Location = new System.Drawing.Point(1008, 92);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 26);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(1120, 569);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.serialData);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox3);
+            this.Name = "Form1";
+            this.Text = " Analog_Sensor v  (0X0)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox serialData;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Datchik/Datchik/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using rtChart;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+using System.Globalization;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Datchik
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+
+            //чтение портов доступных в системе
+            //бесполезное говно
+            string[] ports = SerialPort.GetPortNames();
+            //Добавление найденных портов в бокс
+            //comboBox1.Items.AddRange(ports);
+            // timer1.Interval = 60000; //1 min
+               timer1.Interval = 3600000; // 1 hour
+            //  timer1.Interval = 1800000; 30 min
+
+            // timer1.Enabled = true;
+
+            // timer1.Tick += new EventHandler(timer1_Tick);
+
+        }
+
+        kayChart serialDataChart;
+        kayChart serialDataChart1;
+        kayChart serialDataChart2;
+       
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            chart1.ChartAreas[0].AxisY.Maximum = 3.0;
+            chart1.ChartAreas[0].AxisY.Minimum = -3.0;
+           
+            Thread.Sleep(5000);
+
+            serialDataChart = new kayChart(chart1, 300);
+            serialDataChart.serieName = "X";
+
+            chart2.ChartAreas[0].AxisY.Maximum = 3.0;
+            chart2.ChartAreas[0].AxisY.Minimum = -3.0;
+         
+            serialDataChart1 = new kayChart(chart2, 300);
+            serialDataChart1.serieName = "Y";
+
+            chart3.ChartAreas[0].AxisY.Maximum = 3.0;
+            chart3.ChartAreas[0].AxisY.Minimum = -3.0;
+           
+            serialDataChart2 = new kayChart(chart3 , 300);
+            serialDataChart2.serieName = "Z";
+
+        }
+
+        MemoryStream userInput = new MemoryStream();
+
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+           // richTextBox3.Clear();
+        }
+
+        static List<byte> sBuffer = new List<byte>();
+
+
+        
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string  a = textBox1.Text;
+           
+           SerialPort aSerialPort = new SerialPort ("COM" + a);
+
+
+
+            aSerialPort.BaudRate = 115200;
+            aSerialPort.Parity = Parity.None;
+            aSerialPort.StopBits = StopBits.One;
+            aSerialPort.DataBits = 8;
+            aSerialPort.Encoding = Encoding.UTF8;
+              aSerialPort.ReceivedBytesThreshold = 1;
+            aSerialPort.DataReceived += new SerialDataReceivedEventHandler(serialDataReceivedEventHandler);
+            aSerialPort.DataReceived += new SerialDataReceivedEventHandler(button1_DoubleClick);
+
+
+
+            // if(textBox.Text == "a")  textBox3 = a;
+
+
+
+
+            //aSerialPort.DataReceived += new SerialDataReceivedEventHandler(serialDataReceivedEventHandler2);//po prikoly
+            // aSerialPort.DataReceived += new SerialDataReceivedEventHandler(serialDataReceivedEventHandler3);//po prikoly
+            aSerialPort.Open();
+           
+            richTextBox3.Visible = false;
+           
+        }
+        public void button1_DoubleClick(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+        }
+
+
+
+        private void serialDataReceivedEventHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+          
+            NumberFormatInfo provider = new NumberFormatInfo();
+/*
+            provider.NumberDecimalSeparator = ",";
+            provider.NumberGroupSeparator = ".";
+            provider.NumberGroupSizes = new int[] { 3 };
+            */
+            NumberFormatInfo provider1 = new NumberFormatInfo();
+            /*
+            provider1.NumberDecimalSeparator = ",";
+            provider1.NumberGroupSeparator = ".";
+            provider1.NumberGroupSizes = new int[] { 3 };
+            */
+
+            NumberFormatInfo provider2 = new NumberFormatInfo();
+            /*
+            provider2.NumberDecimalSeparator = ",";
+            provider2.NumberGroupSeparator = ".";
+            provider2.NumberGroupSizes = new int[] { 3 };
+            */
+
+            SerialPort sData = sender as SerialPort;
+          
+                string recvData1 = sData.ReadLine();
+                double b = Convert.ToDouble(recvData1, provider);
+                double a;
+         
+                if (b > 512)
+                {
+
+                a = (b / 1023.0) * 5.0 / 2.0;
+                    serialData.Invoke((MethodInvoker)delegate { serialData.AppendText("\n" + "X:" + a.ToString("0.000") + "\n"); });
+                    serialDataChart.TriggeredUpdate(a);
+                }
+                else if (b < 487)
+
+                {
+                a = (b / 1023.0) * 5.0 / (-2.0);
+                    serialData.Invoke((MethodInvoker)delegate { serialData.AppendText("\n" + "X:" + a.ToString("0.000") + "\n"); });
+
+                    serialDataChart.TriggeredUpdate(a);
+                }
+                else
+                {
+                    a = b * 0;
+                    serialData.Invoke((MethodInvoker)delegate { serialData.AppendText("\n" + "X:" + a.ToString("0.000") + "\n"); });
+
+                    serialDataChart.TriggeredUpdate(a);
+                }
+
+           
+
+
+                SerialPort sData1 = sender as SerialPort;
+            string recvDataShit = sData1.ReadLine();
+            double b2 = Convert.ToDouble(recvDataShit, provider);
+            //  string recvData2 = sData1.ReadLine();
+
+            double a1;
+            if (b2 > 512)
+            {
+
+                a1 = (b2 / 1023.0) * 5.0 / 2.0;
+               richTextBox1.Invoke((MethodInvoker)delegate { richTextBox1.AppendText("\n" + "Y:" + a1.ToString("0.000") + "\n"); });
+                serialDataChart1.TriggeredUpdate(a1);
+            }
+            else if (b2 < 487)
+
+            {
+                a1 = (b2 / 1023.0) * 5.0 / (-2.0);
+                richTextBox1.Invoke((MethodInvoker)delegate { richTextBox1.AppendText("\n" + "Y:" + a1.ToString("0.000") + "\n"); });
+
+                serialDataChart1.TriggeredUpdate(a1);
+            }
+            else
+            {
+                a1 = b2 * 0;
+                richTextBox1.Invoke((MethodInvoker)delegate { richTextBox1.AppendText("\n" + "Y:" + a1.ToString("0.000") + "\n"); });
+               
+                serialDataChart1.TriggeredUpdate(a1);
+            }
+
+
+            //    }
+
+            SerialPort sData2 = sender as SerialPort;
+
+            string recvData3 = sData2.ReadLine();
+            double b3 = Convert.ToDouble(recvData3, provider);
+            //  string recvData2 = sData1.ReadLine();
+            double a2;
+
+            if (b3 > 512)
+            {
+
+                a2 = (b3 / 1023.0) * 5.0 / 2.0;
+                richTextBox2.Invoke((MethodInvoker)delegate { richTextBox2.AppendText("\n" + "Z:" + a2.ToString("0.000") + "\n"); });
+                serialDataChart2.TriggeredUpdate(a2);
+            }
+            else if (b3 < 487)
+
+            {
+                a2 = (b3 / 1023.0) * 5.0 / (-2.0);
+                richTextBox2.Invoke((MethodInvoker)delegate { richTextBox2.AppendText("\n" + "Z:" + a2.ToString("0.000") + "\n"); });
+
+                serialDataChart2.TriggeredUpdate(a2);
+            }
+            else
+            {
+                 a2 = b3 * 0;
+                richTextBox2.Invoke((MethodInvoker)delegate { richTextBox2.AppendText("\n" + "Z:" + a2.ToString("0.000") + "\n"); });
+
+                serialDataChart2.TriggeredUpdate(a2);
+            }
+                //   }
+             
+                DateTime localDate = DateTime.Now;
+                richTextBox3.Invoke((MethodInvoker)delegate { richTextBox3.AppendText( "\n" +  "  "   + a.ToString("0.000") + "  " +  a1.ToString("0.000") + "  " + a2.ToString("0.000") + "  ");  });
+            // richTextBox3.Invoke((MethodInvoker)delegate { richTextBox3.AppendText("\n" + "Y:" + a1 + "\n"); });
+            // richTextBox3.Invoke((MethodInvoker)delegate { richTextBox3.AppendText("\n" + "Z:" + a2 + "\n"); });
+          
+
+        }
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+            // no
+        }
+
+        private void serialData_TextChanged(object sender, EventArgs e)
+        {
+            serialData.SelectionStart = serialData.Text.Length;
+
+            serialData.ScrollToCaret();
+
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+
+            richTextBox1.ScrollToCaret();
+
+            richTextBox2.SelectionStart = richTextBox2.Text.Length;
+
+            richTextBox2.ScrollToCaret();
+           // richTextBox3.Visible = false;
+           
+     //       richTextBox3.SelectionStart = richTextBox3.Text.Length;
+
+       //     richTextBox3.ScrollToCaret();
+
+        }
+
+
+
+       private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog2_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sFile = new SaveFileDialog();
+
+
+
+            string[] lines = { serialData.Text, richTextBox1.Text, richTextBox2.Text };
+
+
+            sFile.Filter = "rtf files (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (sFile.ShowDialog() == DialogResult.OK &&
+               sFile.FileName.Length > 0)
+            {
+
+                StreamWriter wr = new StreamWriter(sFile.FileName);
+                DateTime ThToday = DateTime.Now;
+
+                string[] readText = richTextBox3.Lines;
+               
+                foreach (var c in readText)
+                {
+
+                    wr.WriteLine(c);
+                }
+
+                wr.Close();
+
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            richTextBox3.Visible = true;
+            serialDataChart.TriggeredUpdate(0);
+            serialDataChart1.TriggeredUpdate(0);
+            serialDataChart2.TriggeredUpdate(0);
+
+        }
+    }
+
+      
+    }
+
+
+
+
+        
+ 
